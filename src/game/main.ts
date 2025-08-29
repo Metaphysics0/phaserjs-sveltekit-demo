90b8a5eacf15b8292250a4bbc5d7dc4d712f454b
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-29 10:32:02 +0300

    feat: basic collision

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,21 +1,25 @@
-import { Game as MainGame } from './scenes/Game';
-import { AUTO, Game, Types } from 'phaser';
+import { Game as MainGame } from "./scenes/Game";
+import { Game, type Types } from "phaser";
 
 // Find out more information about the Game Config at:
 // https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Types.Core.GameConfig = {
-    type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        MainGame
-    ]
+  type: Phaser.CANVAS,
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300, x: 0 },
+      debug: false,
+    },
+  },
+  scene: [MainGame],
 };
 
 const StartGame = (parent: string) => {
-    return new Game({ ...config, parent });
-}
+  return new Game({ ...config, parent });
+};
 
 export default StartGame;
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,34 +1,70 @@
-import { Scene } from 'phaser';
-import { EventBus } from '../EventBus';
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
-    constructor ()
-    {
-        super('Game');
-    }
+export class Game extends Scene {
+  constructor() {
+    super("Game");
+  }
 
-    preload ()
-    {
-        this.load.setPath('assets');
-        
-        this.load.image('star', 'star.png');
-        this.load.image('background', 'bg.png');
-        this.load.image('logo', 'logo.png');
-    }
+  preload() {
+    this.load.setPath("assets");
 
-    create ()
-    {
-        
-        this.add.image(512, 384, 'background');
-        this.add.image(512, 350, 'logo').setDepth(100);
-        this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-        
-        EventBus.emit('current-scene-ready', this);
+    this.load.image("star", "star.png");
+    this.load.image("ground", "platform.png");
+    this.load.image("sky", "sky.png");
+    this.load.image("bomb", "bomb.png");
+    this.load.spritesheet("dude", "dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
 
-    }
+  create() {
+    this.add.image(400, 300, "sky");
+
+    const platforms = this.createPlatforms();
+    const player = this.createPlayer();
+
+    this.physics.add.collider(player, platforms);
+  }
+
+  private createPlatforms() {
+    const platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 220, "ground");
+
+    return platforms;
+  }
+
+  private createPlayer() {
+    const player = this.physics.add.sprite(100, 450, "dude");
+    player.setGravityY(300);
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    return player;
+  }
 }
