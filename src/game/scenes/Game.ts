a497e10c3d26d9faba1c216a0e6f10a89e09655b
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-29 12:25:35 +0300

    feat: basic bomb collision

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -5,12 +5,16 @@ type Player = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 type Platforms = Phaser.Physics.Arcade.StaticGroup;
 type Stars = Phaser.Physics.Arcade.Group;
 export class Game extends Scene {
-  player?: Player;
-  platforms?: Platforms;
-  stars?: Stars;
+  player!: Player;
+  platforms!: Platforms;
+  stars!: Stars;
+  bombs!: Phaser.Physics.Arcade.Group;
+  cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
-  score: number = 0;
-  scoreText?: Phaser.GameObjects.Text;
+  score = 0;
+  scoreText!: Phaser.GameObjects.Text;
+
+  gameOver: boolean = false;
 
   constructor() {
     super("Game");
@@ -22,15 +26,26 @@ export class Game extends Scene {
 
   create() {
     this.add.image(400, 300, "sky");
+
     this.platforms = this.createPlatforms();
     this.player = this.createPlayer();
     this.stars = this.createStars();
-    this.scoreText = this.add.text(16, 16, "score: 0", {
-      fontSize: "32px",
-    });
+    this.bombs = this.physics.add.group();
+    this.scoreText = this.add.text(16, 16, "score: 0", { fontSize: "32px" });
+    this.cursors = this.input.keyboard!.createCursorKeys();
 
     this.physics.add.collider(this.player, this.platforms);
     this.physics.add.collider(this.stars, this.platforms);
+    this.physics.add.collider(this.bombs, this.platforms);
+
+    this.physics.add.collider(
+      this.player,
+      this.bombs,
+      this.hitBomb,
+      undefined,
+      this
+    );
+
     this.physics.add.overlap(
       this.player,
       this.stars,
@@ -41,7 +56,7 @@ export class Game extends Scene {
   }
 
   update() {
-    this.createKeyboardInput();
+    this.handleInput();
   }
 
   private createPlatforms() {
@@ -84,6 +99,12 @@ export class Game extends Scene {
 
     return player;
   }
+  private hitBomb = () => {
+    this.physics.pause();
+    this.player.setTint(0xff0000);
+    this.player.anims.play("turn");
+    this.gameOver = true;
+  };
 
   private createStars() {
     const stars = this.physics.add.group({
@@ -99,25 +120,43 @@ export class Game extends Scene {
     return stars;
   }
 
-  private collectStar(player: any, star: any) {
+  private collectStar: Phaser.Types.Physics.Arcade.ArcadePhysicsCallback = (
+    _obj1,
+    obj2
+  ) => {
+    const star = obj2 as Phaser.Physics.Arcade.Sprite;
     star.disableBody(true, true);
     this.score += 10;
-    if (!this.scoreText) return;
     this.scoreText.setText("Score: " + this.score);
-  }
 
-  private createKeyboardInput() {
-    const cursors = this.input.keyboard?.createCursorKeys();
-    if (!cursors) return;
-    if (!this.player) {
-      console.warn("[createKeyboardInput] - Player not loaded yet");
-      return;
+    if (this.stars.countActive(true) === 0) {
+      this.stars.children.iterate((child: Phaser.GameObjects.GameObject) => {
+        const s = child as Phaser.Physics.Arcade.Image;
+        s.enableBody(true, s.x, 0, true, true);
+        return null;
+      });
+
+      const x =
+        this.player.x < 400
+          ? Phaser.Math.Between(400, 800)
+          : Phaser.Math.Between(0, 400);
+      const bomb = this.bombs.create(
+        x,
+        16,
+        "bomb"
+      ) as Phaser.Physics.Arcade.Image;
+      bomb
+        .setBounce(1)
+        .setCollideWorldBounds(true)
+        .setVelocity(Phaser.Math.Between(-200, 200), 20);
     }
+  };
 
-    if (cursors.left.isDown) {
+  private handleInput() {
+    if (this.cursors.left.isDown) {
       this.player.setVelocityX(-160);
       this.player.anims.play("left", true);
-    } else if (cursors.right.isDown) {
+    } else if (this.cursors.right.isDown) {
       this.player.setVelocityX(160);
       this.player.anims.play("right", true);
     } else {
@@ -125,8 +164,8 @@ export class Game extends Scene {
       this.player.anims.play("turn");
     }
 
-    if (cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-440);
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-500);
     }
   }
 }
